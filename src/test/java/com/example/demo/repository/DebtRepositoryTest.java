2f71c6e37f13d6a6971de1df05d6794a54faf8c8
Mikhail Usatenko <usatenkomihailh@gmail.com>
2021-05-26 01:32:39 +0300

    Added test classes for repositories

---
+++ src/test/java/com/example/demo/repository/DebtRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DebtRepositoryTest {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@AfterEach
+	void tearDown() {
+	}
+}
